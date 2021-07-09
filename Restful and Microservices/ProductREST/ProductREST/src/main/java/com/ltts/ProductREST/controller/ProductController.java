c47ee0de388ef8c94d4aff297fafb9bfe51da9fb
Vaibhav Gaur <VaibhavGaur56789@gmail.com>
2021-07-09 16:45:14 +0530

    Database Connected

---
+++ Restful and Microservices/ProductREST/ProductREST/src/main/java/com/ltts/ProductREST/controller/ProductController.java
@@ -0,0 +1,50 @@
+package com.ltts.ProductREST.controller;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.ltts.ProductREST.model.Product;
+
+@RestController
+public class ProductController {
+	
+	@Autowired
+	private ProductService service;
+	
+	@GetMapping("/products")
+	public List<Product> list() {
+		return service.listAll();
+	}
+	
+	@GetMapping("/products/{id}")
+	public Product get(@PathVariable int id) {
+		return service.get(id);
+	}
+	
+	@PostMapping("/products")
+	public void add(@RequestBody Product product) {
+		service.save(product);
+	}
+	
+	@PutMapping("/products/{id}")
+	public ResponseEntity<?> update(@RequestBody Product product, @PathVariable int id) {
+		try {
+		Product existProduct = service.get(id);
+		service.save(product);
+		return new ResponseEntity<>(HttpStatus.OK);
+	} catch(NoSuchElementException e) {
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+}
